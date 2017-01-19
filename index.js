27a3af372a3b7e7be44ebfa58491c4aee76e79e0
jcros8 <jack.cross1@aexp.com>
2017-01-19 17:35:14 -0500

    fix(plugins): remove plugins from index.js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ module.exports = {
   extends: [
     'eslint-config-airbnb',
   ].map(require.resolve),
-  plugins: [
-    'import',
-    'jsx-a11y',
-    'react'
-  ],
   rules: {
     // open a PR per rule change
     // ****.com/stash/projects/UIE/repos/eslint-config-axp/pull-requests?create
