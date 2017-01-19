8c2d6d39a8380e00876ab35622ff90cc029f6d91
jcros8 <jack.cross1@aexp.com>
2017-01-19 17:24:32 -0500

    feat(airbnb): Upgraded eslint-config-airbnb

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ module.exports = {
   extends: [
     'eslint-config-airbnb',
   ].map(require.resolve),
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react'
+  ],
   rules: {
     // open a PR per rule change
     // ****.com/stash/projects/UIE/repos/eslint-config-axp/pull-requests?create
+    'comma-dangle': 0,
+    'react/jsx-no-bind': [2, {
+      'ignoreRefs': true
+    }]
   }
 };
