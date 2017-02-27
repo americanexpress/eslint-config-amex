dc19c72c06f04eeaf18494a9a58561fa1f1d0503
Andres Escobar <Andres.Escobar@aexp.com>
2017-02-26 20:47:21 -0700

    feature(testConfig): adding jest specific configurations

--- test.js
+++ test.js
@@ -2,8 +2,15 @@ module.exports = {
   extends: [
     './index',
   ].map(require.resolve),
+  env: {
+    jest: true,
+  },
+  plugins: [
+    'jest',
+  ],
   rules: {
     // open a PR per rule change
     // ****.com/stash/projects/UIE/repos/eslint-config-axp/pull-requests?create
+    'global-require': 'warn',
   },
 };
