afb9f7e4833976ae5859e95ea864a54f71a0cbe0
Andres Escobar <Andres.Escobar@aexp.com>
2017-02-27 09:17:26 -0700

    feature(testConfig): adding reasoning for each rule

--- test.js
+++ test.js
@@ -11,9 +11,15 @@ module.exports = {
   rules: {
     // open a PR per rule change
     // ****.com/stash/projects/UIE/repos/eslint-config-axp/pull-requests?create
+
+    // following 4 rules all added in PR 14
+    // the way jest mocking works sometimes requires requiring within scope of each test
     'global-require': 'warn',
+    // skipped tests can lead to false build successes
     'jest/no-disabled-tests': 'error',
+    // forces you to remove .only from tests by raising error whenever you are using that feature.
     'jest/no-focused-tests': 'error',
+    // disallows having tests with identical name
     'jest/no-identical-title': 'error',
   },
 };
