733c090cbdf7d7230307a1daa4d5e7b60b205456
Scott McIntyre <scott.mcintyre@aexp.com>
2024-11-15 05:44:02 -0500

    feat: support typescript extensions (#131)
    Co-authored-by: Jamie King <jamie.king@aexp.com>

--- index.js
+++ index.js
@@ -189,15 +189,15 @@ module.exports = {
     // Forbid the use of extraneous packages
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: [
-        // base list from airbnb config
+        // base list from airbnb config + typescript extensions
         'test/**', // tape, common npm pattern
         'tests/**', // also common npm pattern
         'spec/**', // mocha, rspec-like pattern
         '**/__tests__/**', // jest pattern
         '**/__mocks__/**', // jest pattern
-        'test.{js,jsx}', // repos with a single test file
-        'test-*.{js,jsx}', // repos with multiple top-level test files
-        '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
+        'test.{js,jsx,ts,tsx}', // repos with a single test file
+        'test-*.{js,jsx,ts,tsx}', // repos with multiple top-level test files
+        '**/*{.,_}{test,spec}.{js,jsx,ts,tsx}', // tests where the extension or filename suffix denotes that it is a test
         '**/jest.config.js', // jest config
         '**/jest.setup.js', // jest setup
         '**/vue.config.js', // vue-cli config
